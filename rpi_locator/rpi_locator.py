cf35a674eb0235a91167065993e45e86308c6924
Jeffrey Bragdon <jbragdon@pindrop.com>
2023-01-07 20:49:55 -0500

    Finally can figure out its cidr subnet notation

--- rpi_locator/rpi_locator.py
+++ rpi_locator/rpi_locator.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 from scapy.all import *
-import socket, urllib3, re, os, time, shutil, codecs, psutil
+import urllib3, re, os, time, shutil, codecs, subprocess
 
 CACHE_TIME = 86400  # 24 hours expressed in seconds
 CACHE_FILE = os.path.join(os.getcwd(), ".oui-cache")
 COMPANY_NAME = 'Raspberry'
 url = 'https://standards-oui.ieee.org/oui/oui.txt'
+
+def get_network_cidr():
+    # Get the ethernet adapter name
+    output = subprocess.run(["ip", "route", "get", "1"], capture_output=True)
+    output_str = output.stdout.decode("utf-8")
+    ip_address = output_str.split()[6]
+    default_iface = output_str.split()[4]
+    
+    cmd = f"ip route | grep src | grep {default_iface} | awk '{{print $1}}'"
+    ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+    output = ps.communicate()[0]
+    return(output.decode('utf-8'))
+    
 
 def mac_to_oui(m):
     oui = "-".join(m.split(':')[:3])
@@ -21,6 +34,7 @@ def output_identifiers(file, string):
                     matches.append(line[0:8])
     return(matches)
 
+
 def update_cache(u):
     http = urllib3.PoolManager()
     r = http.request('GET', u, preload_content=False)
@@ -30,6 +44,7 @@ def update_cache(u):
 
         r.release_conn()
     print('Cache updated.')
+
 
 if __name__ == "__main__":
     print(f'Looking for raspberri pis.')
@@ -46,7 +61,10 @@ if __name__ == "__main__":
     
     matching_oui_list = output_identifiers(CACHE_FILE, COMPANY_NAME)
 
-    ans,unans = arping("10.23.23.0/24", verbose=0)
+    ignore_ifaces_list = ['lo', 'docker0']
+    network_cidr = get_network_cidr()
+    ans,unans = arping(network_cidr.strip(), verbose=0)
+    print(f"List of located RPIs on network: {network_cidr}")
     for s,r in ans:
         if mac_to_oui(r[Ether].src) in matching_oui_list:
             print("{} {}".format(r[Ether].src,s[ARP].pdst))
